770ffe7d426090f7f73b624afd0e3ea550802585
alebar12 <abarducci@proton.me>
2026-09-11 08:49:23 +0200

    chore: enlarged meal column

--- src/Objects.ts
+++ src/Objects.ts
@@ -5,7 +5,7 @@ export const menuIdRequestSchema = z.object({ menuId: z.string() });
 export const mealRequestSchema = z.object({
     day: z.iso.date(),
     mealType: z.enum(["LUNCH", "DINNER"]),
-    meal: z.string().max(256),
+    meal: z.string().max(1024),
 });
 
 export type Meal = z.infer<typeof mealRequestSchema>;
